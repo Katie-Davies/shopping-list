4dc73d0225d77152d464d759b1d08474fdc1f36a
Katie Davies <millerkatie1990@gmail.com>
2024-03-03 15:21:54 +1300

    create useState to update lists

--- client/components/ShoppingList.tsx
+++ client/components/ShoppingList.tsx
@@ -6,24 +6,64 @@ function ShoppingList() {
     rightInCupboard: ['coffee', 'sugar', 'jam', 'paper towels'],
   })
 
+  function leftClick(shoppingItem: string): void {
+    //deconstruct the arrays
+    const { leftToBuy, rightInCupboard } = list
+
+    setList({
+      //remove item from left list
+      leftToBuy: leftToBuy.filter((listItem) => listItem !== shoppingItem),
+      //add to right list
+      rightInCupboard: [...rightInCupboard, shoppingItem],
+    })
+  }
+
+  function rightClick(shoppingItem: string): void {
+    //deconstruct the arrays
+    const { leftToBuy, rightInCupboard } = list
+
+    setList({
+      //remove item from right list
+      rightInCupboard: rightInCupboard.filter(
+        (listItem) => listItem !== shoppingItem
+      ),
+      //add to left list
+      leftToBuy: [...leftToBuy, shoppingItem],
+    })
+  }
+
   return (
     <>
       <section className="container">
         <section className="listcontainer">
           <ul>
+            <h2>To buy</h2>
             {list.leftToBuy.map((shoppingItem) => (
               <li key={shoppingItem}>
                 {shoppingItem}
-                <button>Check</button>
+                <button
+                  onClick={() => {
+                    leftClick(shoppingItem)
+                  }}
+                >
+                  Check
+                </button>
               </li>
             ))}
           </ul>
 
           <ul>
+            <h2>In the Cart</h2>
             {list.rightInCupboard.map((shoppingItem) => (
               <li key={shoppingItem}>
                 {shoppingItem}
-                <button>run out</button>
+                <button
+                  onClick={() => {
+                    rightClick(shoppingItem)
+                  }}
+                >
+                  run out
+                </button>
               </li>
             ))}
           </ul>
